146b20c7771a8c0ede50303696da3ff67cf11f0e
mojisleep <2947197446@qq.com>
2024-04-19 19:07:53 +0800

    第八次提交

---
+++ test_7/test_7/test_7.c
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int i = 1;//初始化
+//	while (i<=10)//判断
+//	{
+//		if (i == 5)
+//			continue;
+//
+//		printf("%d ", i);
+//		i++;//调整
+//	}
+//
+//	return 0;
+//}
+ //continue会跳过后面的代码,break会终止程旭
+
+//打印1~10
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			continue;
+//
+//		printf("%d ", i);
+//		i = 12;
+//	}
+//	
+//	return 0;
+//}
+//
+//int main()
+//{
+//	//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	////0~9
+//	//int i = 0;
+//	//for (i = 0; i < 10; i++)
+//	//{
+//	//	printf("%d ", arr[i]);
+//	//}
+//
+//	//int i = 0;
+//	//for (i = 100; i <= 200; i++)
+//	//{
+//	//}
+//
+//	//for循环的判断部分省略意味这判断会恒成立
+//	//for (;;)
+//	//{
+//	//	printf("hehe\n");
+//	//}
+//
+//	//int i = 0;
+//	//for (i = 0; i < 10; i++)
+//	//{
+//	//	printf("hehe\n");
+//	//}
+//
+//	//int i = 0;
+//	//int j = 0;
+//
+//	//for (i=0; i < 3; i++)
+//	//{
+//	//	for (i=0; j < 3; j++)
+//	//	{
+//	//		printf("hehe\n");
+//	//	}
+//	//}
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("hehe\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//
+//	do 
+//	{
+//		if (i == 5)
+//			continue;
+//
+//		printf("%d ", i);
+//		i++;
+//	}	
+//	while (i <= 10);
+//
+//	return 0;
+//}
