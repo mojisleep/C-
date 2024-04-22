0abd277df97b20acbdf22be160853a740f327239
mojisleep <2947197446@qq.com>
2024-04-22 17:22:32 +0800

    第九次提交

--- test_7/test_7/test_7.c
+++ test_7/test_7/test_7.c
@@ -14,7 +14,7 @@
 //
 //	return 0;
 //}
- //continue会跳过后面的代码,break会终止程旭
+ //continue会跳过后面的代码,break会终止程序
 
 //打印1~10
 
@@ -96,3 +96,146 @@
 //
 //	return 0;
 //}
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		printf("%d ", i);
+//		i++;
+//	} 
+//	while (i <= 10);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int ret = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		ret = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret = ret * i;
+//		}
+//		sum =sum+ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 1;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		ret = ret * n;
+//		sum = sum + ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 10;
+//	int i = 0;
+//	int az = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < az; i++)
+//	{
+//		if (arr[i] == k)
+//		{
+//			printf("找到了%d\n",i);
+//			break;
+//		}
+//	}
+//	if (i==az)
+//	{
+//		printf("没找到");
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 10;
+
+	//在一个有序数组中查找具体的某个数字n
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+
+	int left = 0;
+	int right = sz - 1;
+
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			printf("找到了，下标是:%d\n", mid);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("找不到\n");
+	}
+
+	return 0;
+}
